9ea3a11d3573474fa703e547ac1086de8b735063
febreeze <sayhello@hvlmnns.de>
2023-05-26 18:38:42 +0200

    speed

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 #define FORCE_NKRO
 #define TAPPING_TERM_PER_KEY
-#define TAPPING_TERM_FAST 110
+#define TAPPING_TERM_FAST 120
 #define TAPPING_TERM 200
 
 #define USB_POLLING_INTERVAL_MS  1
@@ -12,11 +12,11 @@
     #define RGB_MATRIX_LED_COUNT 42
 
     #define RGB_DISABLE_WHEN_USB_SUSPENDED     // turn off effects when suspended
-    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180  // limits maximum brightness of LEDs to x out of 255. If not defined maximum brightness is set to 255
+    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255  // limits maximum brightness of LEDs to x out of 255. If not defined maximum brightness is set to 255
 
-    #define RGBLIGHT_HUE_STEP 8
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_HUE_STEP 1
+    #define RGBLIGHT_SAT_STEP 1
+    #define RGBLIGHT_VAL_STEP 1
 
     #define RGB_MATRIX_KEYPRESSES              // enable key press effects
 
