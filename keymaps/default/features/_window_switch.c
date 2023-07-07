8f8d028ee6403a67ccb3012fa8d0edd484311e10
shvlmnns <sh@dasistweb.de>
2023-07-07 14:08:44 +0200

    adjustments

--- keymaps/default/features/_window_switch.c
+++ keymaps/default/features/_window_switch.c
@@ -10,7 +10,7 @@ void window_switch(uint16_t keycode, keyrecord_t* record) {
 
     if (record->event.pressed) {
         if (keycode == KC_TAB) {
-            if ((mods & MOD_MASK_ALT) || (mods & MOD_MASK_GUI)) {
+            if (mods & (MOD_MASK_ALT || mods & MOD_MASK_GUI || mods & MOD_MASK_CTRL)) {
                 layer_move(LOWER);
                 is_in_window_switch = true;
             }
@@ -18,8 +18,9 @@ void window_switch(uint16_t keycode, keyrecord_t* record) {
     } else if (is_in_window_switch) {
         bool alt_down = keycode == KC_LALT || keycode == KC_RALT;
         bool gui_down = keycode == KC_LGUI || keycode == KC_RGUI;
+        bool ctrl_down = keycode == KC_LCTL || keycode == KC_LCTL;
 
-        if (alt_down || gui_down) {
+        if (alt_down || gui_down || ctrl_down) {
             layer_move(BASE);
             is_in_window_switch = false;
         }
