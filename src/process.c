c3c342a3bbb87bb5da3a322f89488597843036b4
febreeze <sayhello@hvlmnns.de>
2023-11-24 15:11:37 +0100

    update

--- src/process.c
+++ src/process.c
@@ -20,28 +20,26 @@ typedef union {
 
 TabbingState tabbing_state = {};
 
-void get_tabbing_mod(uint16_t kc, keyrecord_t *rec) {
+void handle_tabbing(uint16_t kc, keyrecord_t *rec) {
   bool pressed = rec->event.pressed;
+
   if (kc == KC_LCTL || kc == KC_RCTL) tabbing_state.ctl = pressed;
   if (kc == KC_LGUI || kc == KC_RGUI || (kc == CK_GUI && OS == OS_REM)) tabbing_state.gui = pressed;
   if (kc == KC_LALT || kc == KC_LALT || (kc == CK_ALT && OS == OS_REM)) tabbing_state.alt = pressed;
   
   tabbing_state.any = tabbing_state.ctl || tabbing_state.gui || tabbing_state.alt;
-}
 
-void handle_tabbing(uint16_t kc, keyrecord_t *rec) {
-  bool pressed = rec->event.pressed;
-
-  if (!tabbing_state.any && tabbing_state.enabled && !pressed) {
-    layer_move(BASE);
-    tabbing_state.enabled = false;
-  }
-
-  if (kc != KC_TAB) return;
-
-  if (tabbing_state.any && !tabbing_state.enabled && pressed) {
+  if (pressed) {
+    if (kc != KC_TAB) return;
+    if (tabbing_state.enabled) return;
+    if (!tabbing_state.any) return;
     layer_move(LOWER);
     tabbing_state.enabled = true;
+  } else {
+    if (!tabbing_state.enabled) return;
+    if (tabbing_state.any) return;
+    layer_move(BASE);
+    tabbing_state.enabled = false;
   }
 }
 
@@ -68,7 +66,6 @@ bool process_record_user(uint16_t kc, keyrecord_t *rec) {
   kc = get_current_keycode();
 
   if (handle_rgb(kc, rec)) return false;
-  get_tabbing_mod(kc, rec);
   handle_tabbing(kc, rec);
 
   return can_continue;
